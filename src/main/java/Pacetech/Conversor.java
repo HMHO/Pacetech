69d11f5dafc6626972ff1fa4ffd9906e0c044f1c
Mauricio Herrera <hermaho7@hotmail.com>
2022-08-31 16:46:12 -0300

    Add classe conversora

---
+++ src/main/java/Pacetech/Conversor.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pacetech;
+
+/**
+ *
+ * @author Mauricio
+ */
+public class Conversor {
+    
+    // esta é a classe conversora 
+}
